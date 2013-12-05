9f860466808325ef90d510e7098c5ac3067fff88
Khoa Tran <tvkkpt@gmail.com>
2013-12-05 07:32:27 +0000

    Change "==" to "=" for POSIX compliance

--- git-config.sh
+++ git-config.sh
@@ -12,7 +12,7 @@ git config --global alias.stash-unapply '!git stash show -p | git apply -R'
 printf "Done\n"
 
 # Fix problem non-zero exit code of Vim for Git commit (Mac OS X only)
-if [[ $platform == 'Darwin' ]]; then
+if [ "$platform" = 'Darwin' ]; then
     printf "Fix Vim error for Git commit (Mac OS X only)... "
     git config --global core.editor /usr/bin/vim
     printf "Done\n"
